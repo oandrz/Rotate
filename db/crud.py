684a418de195123cf234dd2fe9f6edee331d26fe
Andreas <oentoro.andreas@gmail.com>
2022-11-24 15:44:06 +0800

    Add Database Impelementation & Add Rotation Implementation
    * Deploy Database Implementation
    
    * Update Argument Name
    
    * Add Add API
    
    * Add Group Process
    
    * Update DB Location
    
    * Add HTTP Code Logic Validation
    
    * Update Version
    
    * Add New API To List Group List
    
    * Add Logger
    
    * Add Logger
    
    * Update Validation
    
    * Remove Query
    
    * Update Crud
    
    * Add List Rotation Logic
    
    * Update Response into String
    
    * Add Response Status Validation
    
    * Add List of Group
    
    * Update Decoration
    
    Co-authored-by: Andreas Dre <andreas.oentoro@foodpanda.com>

---
+++ db/crud.py
@@ -0,0 +1,36 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+
+def getGroupListInChannel(db: Session, channelId: str):
+    return db.query(models.RotationGroup).filter(models.RotationGroup.channelId == channelId).all()
+
+
+def getMemberInGroup(db: Session, groupName: str, channelId: str):
+    return getGroup(db, groupName, channelId).users
+
+
+def getPickedMember(db: Session, groupName: str, channelId: str):
+    return getGroup(db, groupName, channelId).pickedSlackId
+
+
+def getGroup(db: Session, groupName: str, channelId: str):
+    return db.query(models.RotationGroup).filter(
+        models.RotationGroup.name == groupName and models.RotationGroup.channelId == channelId
+    ).first()
+
+
+def createUser(db: Session, user: schemas.UserCreate):
+    db_user = models.User(slackId=user.slackId)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def createGroup(db: Session, group: schemas.GroupCreate):
+    db_group = models.RotationGroup(name=group.name, channelId=group.channelId, pickedSlackId=group.pickedSlackId)
+    db.add(db_group)
+    db.commit()
+    db.refresh(db_group)
+    return db_group
--- main.py
+++ main.py
@@ -1,112 +1,141 @@
 import os
+import requests
+import json
 from slack_bolt import App
-from collections import deque
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends, HTTPException
 from pydantic import BaseModel
 from slack_bolt.adapter.fastapi import SlackRequestHandler
 
+from typing import List
+from sqlalchemy.orm import Session
+from db import crud, models, schemas
+from db.database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+HOST_URL = "https://roundrobinbot-pr-2.onrender.com"
 app = App(
     token=os.environ["SLACK_BOT_TOKEN"],
     signing_secret=os.environ["SLACK_SIGNING_SECRET"]
 )
 
-db = dict()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.command("/add-rotation")
 def add_group(ack, say, command):
     ack()
-    groupName = command['text']
-    print("your group name is", groupName)
-    q = deque()
-    isKeyExist = groupName in db
-    if isKeyExist:
-        q = db[groupName]
-    db[groupName] = q
-    say(f"Success add {groupName}")
+    group_name = command['text']
+    channel_id = command['channel_id']
+    url = HOST_URL + "/group/add"
+    request = {"name": group_name, "channelId": channel_id}
+    response = requests.post(url, json=request)
+
+    if response.status_code == requests.codes.ok:
+        say(f"Success add {group_name}")
+    elif response.status_code == 400:
+        say(f"{group_name} already exist in our database, please try with another user name")
+    else:
+        say(f"Sorry there's an unrecognizable error in my system, please wait until my engineer fix me")
 
 
 @app.command("/add-member")
 def add_member(ack, say, command):
     ack()
     commandText = command['text'].split()
-    groupName = commandText[0]
-    isKeyExist = groupName in db
-    if not isKeyExist:
-        say(f"Group doesn't exist")
-    else:
-        q = db[groupName]
-        numberOfMember = len(commandText)
-        for i in range(1, numberOfMember):
-            q.append(commandText[i])
-        say(f"Add member success")
+    # groupName = commandText[0]
+    # isKeyExist = groupName in db
+    # if not isKeyExist:
+    #     say(f"Group doesn't exist")
+    # else:
+    #     q = db[groupName]
+    #     numberOfMember = len(commandText)
+    #     for i in range(1, numberOfMember):
+    #         q.append(commandText[i])
+    say(f"Add member success")
 
 
 @app.command("/list-member")
 def list_member(ack, say, command):
     ack()
-    commandText = command['text'].split()
-    groupName = commandText[0]
-    isKeyExist = groupName in db
-    if not isKeyExist:
-        say(f"Group doesn't exist")
-    else:
-        q = db[groupName]
-
-        count = 1
-        for member in q:
-            say(f"Group member number {count}: <{member}>!")
-            count += 1
+    # commandText = command['text'].split()
+    # groupName = commandText[0]
+    # isKeyExist = groupName in db
+    # if not isKeyExist:
+    #     say(f"Group doesn't exist")
+    # else:
+    #     q = db[groupName]
+    #
+    #     count = 1
+    #     for member in q:
+    #         say(f"Group member number {count}: <{member}>!")
+    #         count += 1
 
 
 @app.command("/list-rotation")
-def list_rotation(ack, say):
+def list_rotation(ack, say, command):
     ack()
-    if len(db) == 0:
-        say("You don't have saved rotation")
+    channel_id = command['channel_id']
+    url = HOST_URL + "/group/" + channel_id
+    response = requests.get(url)
+
+    if response.status_code == requests.codes.ok:
+        group_list = json.loads(response.text)
+        if len(group_list) == 0:
+            say(f"We don't found any rotation group, please create a new one first.")
+        else:
+            text = ""
+            count = 0
+            for group in group_list:
+                count += 1
+                if count > 1:
+                    text += ', '
+                text += group['name']
+            say(f"Here are the group that I could found in this channel: {text}")
+    elif response.status_code == 400:
+        say(f"We don't found any rotation group, please create a new one first.")
     else:
-        text = ''
-        count = 0
-        for key in db.keys():
-            count += 1
-            if count > 1:
-                text += ', '
-            text += key
-        say(f"Here are the list of your group: {text}")
+        say(f"Sorry there's an unrecognizable error in my system, please wait until my engineer fix me")
 
 
 @app.command("/peek-current")
 def peek_current_turn(ack, say, command):
     ack()
     commandText = command['text'].split()
-    groupName = commandText[0]
-    isKeyExist = groupName in db
-    if not isKeyExist:
-        say("Group doesn't exist")
-    else:
-        q = db[groupName]
-        currentTurn = q[0]
-
-        if currentTurn is not None:
-            say(f"Current turn is <{currentTurn}>!")
+    # groupName = commandText[0]
+    # isKeyExist = groupName in db
+    # if not isKeyExist:
+    #     say("Group doesn't exist")
+    # else:
+    #     q = db[groupName]
+    #     currentTurn = q[0]
+    #
+    #     if currentTurn is not None:
+    #         say(f"Current turn is <{currentTurn}>!")
 
 
 @app.command("/rotate")
 def rotate_member(ack, say, command):
     ack()
     commandText = command['text'].split()
-    groupName = commandText[0]
-    isKeyExist = groupName in db
-    if not isKeyExist:
-        say("Group doesn't exist")
-    else:
-        q = db[groupName]
-        rotated = q.popleft()
-        current = q[0]
-        q.append(rotated)
-
-        if current is not None:
-            say(f"Current turn is <{current}>!")
+    # groupName = commandText[0]
+    # isKeyExist = groupName in db
+    # if not isKeyExist:
+    #     say("Group doesn't exist")
+    # else:
+    #     q = db[groupName]
+    #     rotated = q.popleft()
+    #     current = q[0]
+    #     q.append(rotated)
+    #
+    #     if current is not None:
+    #         say(f"Current turn is <{current}>!")
 
 
 # FastAPI
@@ -159,3 +188,20 @@ async def list_rotation_command(req: Request):
 @fastApp.post("/slack/rotate")
 async def list_rotation_command(req: Request):
     return await app_handler.handle(req)
+
+
+# DB
+@fastApp.post("/group/add")
+async def add_new_group(group: schemas.GroupCreate, db: Session = Depends(get_db)):
+    dbGroup = crud.getGroup(db, groupName=group.name, channelId=group.channelId)
+    if dbGroup:
+        raise HTTPException(status_code=400, detail="Group Already Exist")
+    return crud.createGroup(db=db, group=group)
+
+
+@fastApp.get("/group/{channel_id}", response_model=List[schemas.Group])
+async def get_group_list(channel_id: str, db: Session = Depends(get_db)):
+    dbGroup = crud.getGroupListInChannel(db=db, channelId=channel_id)
+    if dbGroup is None:
+        raise HTTPException(status_code=400, detail="No Group Exist")
+    return dbGroup
